99a90c22851576be1bac2b49088417a5b5c964f9
KJW <johnson9710@gmail.com>
2020-04-04 20:30:17 -0700

    navBar links

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,7 +4,7 @@ import SignupContainer from '../session_form/signup_container';
 import SigninStatusContainer from '../status/signin_status_container';
 import SearchBar from '../search/search_container';
 import ModalForm from '../modal';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 const NavBar = ({modal, hideModal}) => {
 
@@ -24,10 +24,10 @@ const NavBar = ({modal, hideModal}) => {
 
             <div className="trending-bar-under-nav">
                 <span className="trending-text">TRENDING MOVIES:</span>
-                <Link to='movies/210' className="trending-link">Star Wars: The Rise of Skywalker</Link>
-                <Link to='movies/194' className="trending-link">The Dark Knight</Link>
-                <Link to='movies/202' className="trending-link">Joker</Link>
-                <Link to='movies/193' className="trending-link">Cats</Link>
+                <Redirect to='movies/210' className="trending-link">Star Wars: The Rise of Skywalker</Redirect>
+                <Redirect to='movies/194' className="trending-link">The Dark Knight</Redirect>
+                <Redirect to='movies/202' className="trending-link">Joker</Redirect>
+                <Redirect to='movies/193' className="trending-link">Cats</Redirect>
             </div>
 
         </div>
